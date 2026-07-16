bc7c4b52a849615a5a5f9070e6df06a40bcfcaea
M. Hammad Ali <www.hammad622h@gmail.com>
2026-07-16 20:29:17 +0500

    Added theme button

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import ThemeToggle from "./ThemeToggle"
+const Navbar = () => {
+    return (
+        <>
+        <h1>Navbar</h1>
+        <ThemeToggle />
+        </>
+    )
+}
+export default Navbar;
